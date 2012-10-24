a74e67bd9812920b852628ddbdcb9ec8d72b8408
Noriyuki Hosaka <bgnori@gmail.com>
2012-10-24 17:28:10 +0900

    isbn checkers

--- loader.py
+++ loader.py
@@ -18,10 +18,20 @@ googlexmlpath = {
 def isbn_strip(s):
     """
     >>> isbn_strip("978-0-306-40615")
-    '978030640615'
+    (9, 7, 8, 0, 3, 0, 6, 4, 0, 6, 1, 5)
     >>> isbn_strip("0-201-53082-1")
-    '0201530821'
+    (0, 2, 0, 1, 5, 3, 0, 8, 2, 1)
     """
+    def nums():
+        for x in s:
+            try:
+                i = int(x)
+            except:
+                continue
+            if str(i) == x:
+                yield i
+    return tuple(nums())
+
 
 def ISBN13CheckDigit(s):
     """
@@ -34,6 +44,10 @@ def ISBN13CheckDigit(s):
     >>> ISBN13CheckDigit("978-0-306-40615")
     3
     """
+    ns = isbn_strip(s)
+    w = (1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3)
+    return 10 - (sum(map(lambda p : p[0] *p[1], zip(w, ns))) % 10 )
+
 
 def validateISBN10(s):
     """
@@ -42,7 +56,12 @@ def validateISBN10(s):
     The sum of products is 0*10 + 2*9 + 0*8 + 1*7 + 5*6 + 3*5 + 0*4 + 8*3 + 2*2 + 1*1 = 99 = 0 modulo 11. 
     So the ISBN is valid.
     >>> validateISBN10("0-201-53082-1")
+    True
     """
+    ns = isbn_strip(s)
+    w = list(range(1, 11))
+    w.reverse()
+    return sum(map(lambda p: p[0]*p[1], zip(w, ns))) % 11 == 0
 
 
 class Book:
@@ -138,7 +157,7 @@ if __name__ == "__main__":
         print unicode(b)
         print b
 
-else:
+if False:
     with file("test.csv", 'w') as f:
         Book.write_csv(bs, f)
 
