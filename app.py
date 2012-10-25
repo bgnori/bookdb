a63a7f47c2d751b2e11522e19c766c561b0b0c16
Noriyuki Hosaka <bgnori@gmail.com>
2012-10-25 20:15:18 +0900

    added some application feature

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# coding=utf-8
 
 
 import model
@@ -6,6 +8,8 @@ import loader
 import codecs
 import sys
 sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
+
+import re
 
 class CUIApp:
     """
@@ -16,6 +20,7 @@ class CUIApp:
     def __init__(self):
         self.bs = []
         self.filename = "test.csv"
+        self.marked = []
 
     def load(self):
         with file(self.filename) as f:
@@ -24,6 +29,9 @@ class CUIApp:
     def save(self):
         with file(self.filename, 'w') as f:
             loader.write_csv(self.bs, f)
+
+    def sync(self):
+        self.save()# FIXME incremental write
 
     def listall(self):
         for b in self.bs:
@@ -36,11 +44,50 @@ class CUIApp:
             b = model.Book(isbn, title)
         except libisbn.BadISBNException:
             pass
+        self.bs.append(b)
+        self.sync()
 
+    def find(self, s):
+        #FIXME
+        r = re.compile(s, re.UNICODE)
+        with file(self.filename) as f:
+            for found in r.findall(f.read()):
+                print unicode(found, "utf-8")#fileencoding!
+
+    def book(self, isbn):
+        pass
+
+    def mark(self, b):
+        self.marked.append(b)
+
+    def unmark(self, b):
+        self.marked.remove(b)
+
+    def showmarked(self):
+        for b in self.marked:
+            print b
+
+    def mkshelf(self, name):
+        if not self.marked:
+            return
+        for b in self.marked:
+            setattr(b, "shelf", name,)
+
+    def selectschelf(self, name):
+        pass
+
+    def rmshelf(self):
+        pass
+
+    
 
 app = CUIApp()
 
 app.load()
-app.listall()
-
+#app.listall()
+#app.find(u"482.*")
+#app.find(u"入門") #FIXME
+app.mark(app.book("4582851037"))
+app.mark(app.book("4873115094"))
+app.showmarked()
 
