b9f8e8c919dfeb4ded156da325dd93f5d19d3ca5
bgnori <bgnori@gmail.com>
2012-11-11 20:48:00 +0900

    figured out meta.

--- foo.py
+++ foo.py
@@ -1,18 +1,4 @@
 
-
-import Rx
-
-
-rx = Rx.Factory({ "register_core_types": True });
-
-
-#rx.learn_type(schema_test_spec['composedtype']['uri'],
-#rx.add_prefix(schema_test_spec['composedtype']['prefix'][0],
-#schema = rx.make_schema(schema_test_spec["schema"])
-#result = schema.check( test_data[source][entry] )
-
-
-
 """
   if filename == 'spec/index.json': continue
   payload = json.loads(file(filename).read())
@@ -38,11 +24,44 @@ rx = Rx.Factory({ "register_core_types": True });
     test_data[ leaf_name ] = {}
 """
 
+#  if filetype == 'schemata':
+#    test_schemata[ leaf_name ] = payload
+
+# schema_names = test_schemata.keys()
+# for schema_name in schema_names:
+# schema_test_spec = test_schemata[ schema_name ]
+
+#rx.learn_type(schema_test_spec['composedtype']['uri'],
+#rx.add_prefix(schema_test_spec['composedtype']['prefix'][0],
+#schema = rx.make_schema(schema_test_spec["schema"])
+
+# schema = rx.make_schema(yaml.load(open("schema.yaml")))
+# from
+# http://stackoverflow.com/questions/6311738/create-a-model-from-yaml-json-on-the-fly
+
+#result = schema.check( test_data[source][entry] )
+import Rx
+import yaml
+
+rx = Rx.Factory({ "register_core_types": True });
+
+for t in rx.type_registry:
+  print t
+
+meta = """{
+    "type": "//any",
+    "of": [
+      {
+        "type"    : "//rec",
+        "required": { "type": "//str" },
+        "rest"    : "//any"
+        },
+      "//str",
+      ]
+    }"""
 
 with file('rx-schema.yaml') as f:
-
-  schema = rx.make_schema('meta')
-  schema.check(f.read())
-
+  schema = rx.make_schema(yaml.load(meta))
+  print schema.check(meta)#f.read())
 
 
