fc4e6b02bd8d52552fd185d7efc5c61dc63b3828
Noriyuki Hosaka <bgnori@gmail.com>
2012-11-15 14:13:16 +0900

    tests for visit

---
+++ test_visit.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import unittest
+import yaml
+import visit
+
+class TestVisitor(unittest.TestCase):
+    def setUp(self):
+        visit.inject(visit.Visitor)
+
+    @unittest.skip("not ready for this")
+    def testRecursive(self):
+        y = yaml.load('''&A [*A] ''')
+
+    def testInt(self):
+        self.assertEqual(yaml.load('1'), 1)
+
+    def testStr0(self):
+        self.assertEqual(yaml.load('abc'), 'abc')
+
+    def testStr1(self):
+        self.assertEqual(yaml.load('"abc"'), 'abc')
+
+    def testStr2(self):
+        self.assertEqual(yaml.load('""'), '')
+
+    def testNone(self):
+        self.assertIsNone(yaml.load(''))
+
+    def testEmptyList(self):
+        self.assertEqual(yaml.load('[]'), [])
+
+    def testOneItemList(self):
+        self.assertIsInstance(yaml.load('[0]'), list)
+        self.assertEqual(yaml.load('[0]'), [0])
+
+    def testTwoItemList(self):
+        self.assertEqual(yaml.load('[0, "a"]'), [0, 'a'])
+
+if False:
+    with file("sample.yaml") as f:
+        y = yaml.load(f.read())
+
+if False:
+    y = yaml.load('''
+        key-foo: 'value-foo'
+        key-bar: 'value-bar'
+        array: [1, 2, 3]''')
+    print y
--- visit.py
+++ visit.py
@@ -8,14 +8,6 @@ class Visitor(object):
         self.loader = loader
 
     def handleScalarNode(self, sn):
-        """
-        >>> yaml.load('1')
-        1
-        >>> yaml.load("'a'")
-        'a'
-        >>> yaml.load('') is None
-        True
-        """
         prefix = u'tag:yaml.org,2002:'
         mapping = {
             'null':lambda x : None,
@@ -26,20 +18,6 @@ class Visitor(object):
         return mapping[tail](sn.value)
 
     def handleSequenceNode(self, sn):
-        """
-        >>> _foo
-        1
-        >>> yaml.load('[]')
-        []
-        >>> isinstance(yaml.load('[]'), list)
-        True
-        >>> yaml.load('[0]')
-        [0]
-        >>> isinstance(yaml.load('[0]'), list)
-        True
-        >>> yaml.load('[0, "a"]')
-        [0, 'a']
-        """
         return [self.dispatch(v) for v in sn.value]
     
     def handleMappingNode(self, sn):
@@ -61,30 +39,18 @@ class Visitor(object):
         assert False
 
 
-def from_yaml(loader, node):
-    print "from_yaml"
-    v = Visitor(node)
-    return v.dispatch(node)
+def inject(vclass):
+    def from_yaml(loader, node):
+        #print "from_yaml"
+        v = vclass(node)
+        return v.dispatch(node)
+    yaml.add_constructor(u"Root", from_yaml)
+    yaml.add_path_resolver(u"Root", [], dict)
+    yaml.add_path_resolver(u"Root", [], list)
 
+# Do I need these?
+#yaml.add_path_resolver(u"Root", [], str)
+#yaml.add_path_resolver(u"Root", [], int)
+#yaml.add_path_resolver(u"Root", [], None)
 
-yaml.add_constructor(u"Root", from_yaml)
-yaml.add_path_resolver(u"Root", [], dict)
-yaml.add_path_resolver(u"Root", [], list)
-
-_foo = 1
-
-if False:
-    with file("sample.yaml") as f:
-        y = yaml.load(f.read())
-
-if False:
-    y = yaml.load('''
-        key-foo: 'value-foo'
-        key-bar: 'value-bar'
-        array: [1, 2, 3]''')
-    print y
-
-if True:
-    y = yaml.load('''&A [*A] ''')
-    print y
 
